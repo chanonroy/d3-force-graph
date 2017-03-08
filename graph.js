a75b90b902f663bf00e0f766137a2f2321705d77
chanonroy <chanonroy@gmail.com>
2017-03-07 22:11:43 -0500

    added dynamic sizes for groups

--- graph.js
+++ graph.js
@@ -1,25 +1,56 @@
-var width = 900,
+var config = {
+  'linkDistance': 100,
+  'charge': -500,
+  'gravity': 0.05
+};
+
+var width = 1200,
     height = 700;
 
+var color = d3.scale.category10();
+
+// Links Array
 var links = [
     { source: 'Cat', target: 'Animal', value: 0.3 },
     { source: 'Dog', target: 'Animal', value: 4 },
-    { source: 'Wolf', target: 'Animal', value: 6},
+    { source: 'Wolf', target: 'Animal', value: 6 },
+    { source: 'Kitten', target: 'Animal', value: 6 },
+    { source: 'Whale', target: 'Animal', value: 6 },
+    { source: 'Max', target: 'Cat', value: 3 },
+    { source: 'Fred', target: 'Cat', value: 6 },
+    { source: 'George', target: 'Cat', value: 6 },
+    { source: 'Marty', target: 'Wolf', value: 6 },
+    { source: 'Vector', target: 'Wolf', value: 16 },
+    { source: 'Spike', target: 'Dog', value: 6 },
+    { source: 'Bruce', target: 'Whale', value: 6 },
+    { source: 'George', target: 'Wolf', value: 6 },
+    { source: 'Billy', target: 'Cat', value: 5},
+    { source: 'Billy', target: 'Dog', value: 5},
+    { source: 'Billy', target: 'Wolf', value: 5}
 ];
 
-// create empty nodes array
-var nodes = {};
+// Create empty nodes array (need to append fixed category items first)
 
-// compute nodes from links data (create new node if needed)
+var nodes = {
+  "Animal": { name: 'Animal', group: 3 },
+  "Wolf": { name: "Wolf", group: 2 },
+  "Dog": { name: "Dog", group: 2 },
+  "Cat": { name: "Cat", group: 2 },
+  "Kitten": { name: "Kitten", group: 2 },
+  "Whale": { name: "Whale", group: 2 },
+};
+
+// Compute nodes from links data (create new node if needed)
 links.forEach(function(link) {
-    link.source = nodes[link.source] || (nodes[link.source] = { name: link.source });
-    link.target = nodes[link.target] || (nodes[link.target] = { name: link.target });
+    link.source = nodes[link.source] || (nodes[link.source] = { name: link.source, group: 1 });
+    link.target = nodes[link.target] || (nodes[link.target] = { name: link.target, group: 1 });
     // link.value = +link.value;
 });
 
-// CANVAS
-var svg =
-  d3.select('body').append('svg')
+console.log(JSON.stringify(nodes));
+
+// Setup Canvas
+var svg = d3.select('.container').append('svg')
     .attr('width', width)
     .attr('height', height);
 
@@ -28,9 +59,10 @@ var force = d3.layout.force()
     .size([width, height])
     .nodes(d3.values(nodes))
     .links(links)
-    .gravity(0.05)
+    .charge(config.charge)
+    .gravity(config.gravity)
     .on("tick", tick)
-    .linkDistance(300)
+    .linkDistance(config.linkDistance)
     .start();
 
 // EDGES
@@ -38,7 +70,6 @@ var link = svg.selectAll('.link')
      .data(links)
      .enter().append('line')
      .attr('class', 'link');
-    //  .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
 // ----------------------------------------------
 
@@ -51,13 +82,12 @@ var node = svg.selectAll('.node')
 
 node.append("circle")
   .attr('class', 'node')
-  .attr('r', width * 0.02);
-
-// node.append("rectangle")
-//   .attr('width', )
+  .attr('r', function(d) { return 10 * d.group; })
+  .style("fill", function(d) { return color(d.group); });
 
 node.append("text")
-  .attr("x", 20)
+  .attr("x", -20) // TODO, make function here to change based on group
+  .attr("y", -40)
   .attr("dy", ".35em")
   .text(function(d) { return d.name; });
 
